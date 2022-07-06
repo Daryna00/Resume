85618cab7d66fd669291758bdc72a462fb00cd7b
Dasha <d.denderuk@gmail.com>
2022-07-06 13:06:38 +0300

    page registration

--- resume_front/src/pages/Registr.js
+++ resume_front/src/pages/Registr.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import {Button, Form} from "react-bootstrap";
 
 const Registr = () => {
     return (
-        <div>
-            REGISTER
-        </div>
+        <Form className='form-login'>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Password confirmation</Form.Label>
+                <Form.Control type="password" placeholder="Password confirmation" />
+            </Form.Group>
+            <Button variant="primary" className="button-submit" type="submit">
+                Submit
+            </Button>
+        </Form>
     );
 };
 
